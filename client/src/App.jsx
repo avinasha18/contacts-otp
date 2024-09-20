d6817ec3c22616e0005ab6fad943272442652df6
Tejassri Avinasha <136706216+avinasha18@users.noreply.github.com>
2024-09-20 22:26:08 +0530

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -8,24 +8,34 @@ import SentMessages from './pages/SentMessages';
 import Sidebar from './components/Sidebar';
 import ContactDetails from './pages/ContactDetails';
 
+// Component for handling protected routes
+const PrivateRoute = ({ element }) => {
+  const token = localStorage.getItem('token');
+  return token ? element : <Navigate to="/login" />;
+};
+
 function App() {
+  const location = useLocation();  // Get the current route location
+  
+  // Determine if the user is on login or register pages
+  const hideSidebar = location.pathname === '/login' || location.pathname === '/register';
+
   return (
-    <Router>
-       <div className="flex flex-col h-screen bg-white text-gray-100 w-full">
-      <Navbar  />
+    <div className="flex flex-col h-screen bg-white text-gray-100 w-full">
+      <Navbar />
       <div className={`h-screen flex flex-1 overflow-hidden no-scrollbar`}>
-        <Sidebar />
+        {/* Render the Sidebar only if not on login or register pages */}
+        {!hideSidebar && <Sidebar />}
         <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/" element={<Contacts />} />
-              <Route path="/contact/:id" element={<ContactDetails />} />
-              <Route path="/sent-messages" element={<SentMessages />} />
-            </Routes>
-          </div>
-        </div>
-     
-    </Router>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {/* Protect these routes */}
+          <Route path="/" element={<PrivateRoute element={<Contacts />} />} />
+          <Route path="/contact/:id" element={<PrivateRoute element={<ContactDetails />} />} />
+          <Route path="/sent-messages" element={<PrivateRoute element={<SentMessages />} />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
